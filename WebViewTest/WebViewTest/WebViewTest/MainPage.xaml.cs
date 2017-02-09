5f2d56ef7523b5a095bad1caef55fceca0b91bdc
Kohinata <kohinata@p-world.co.jp>
2017-02-09 15:23:36 +0900

    メアドを正しく入れると色が変わる

--- WebViewTest/WebViewTest/WebViewTest/MainPage.xaml.cs
+++ WebViewTest/WebViewTest/WebViewTest/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -13,5 +14,34 @@ namespace WebViewTest
         {
             InitializeComponent();
         }
+
+    }
+
+    public class ChkEmailBehavior : Behavior<Entry>
+    {
+        private readonly Regex _regex = new Regex(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
+        public bool IsValid { get; private set; }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            //ハンドラの追加
+            bindable.TextChanged += CheckEmail;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            //ハンドラの削除
+            bindable.TextChanged -= CheckEmail;
+        }
+
+        private void CheckEmail(object sender, TextChangedEventArgs e)
+        {
+            //文字列がEmailとして正しいかどうかのチェック
+            var m = _regex.Match(e.NewTextValue);
+            //正かどうかによってテキストの色を変化させる
+            ((Entry)sender).TextColor = m.Success ? Color.Default : Color.Red;
+            IsValid = m.Success;
+        }
+
     }
 }
